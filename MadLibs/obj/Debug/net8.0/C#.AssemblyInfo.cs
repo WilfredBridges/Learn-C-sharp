3386adaa478082a34422468998b724665eeacc45
Wilfred <wbridges905@gmail.com>
2024-03-11 21:30:41 +0000

    File folder change

--- MadLibs/obj/Debug/net8.0/C#.AssemblyInfo.cs
+++ MadLibs/obj/Debug/net8.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6583daebb1e5ff855523f94dd6dea7bd8d5d687")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33fca24da48ecbc815a7080931918e3cbc9a0aa6")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
